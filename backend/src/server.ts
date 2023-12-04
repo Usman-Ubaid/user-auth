4454ea757a07cd9779d49578dba99739a5ef4865
Welcome ! <usmanubaid9@gmail.com>
2023-12-04 13:29:43 +0100

    folder change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { myEnvVariables } from "./helpers/env";
-import { connection } from "./helpers/dbConnection";
+import { myEnvVariables } from "./db/env";
+import { connection } from "./db/dbConnection";
 
 const app = express();
 
