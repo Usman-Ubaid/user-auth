df59b68b10faed54f8f644498f01d85bfeadf962
Welcome ! <usmanubaid9@gmail.com>
2023-12-04 14:19:43 +0100

    add middleware and userRouter routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { myEnvVariables } from "./db/env";
 import { connection } from "./db/dbConnection";
+import { router } from "./routes/userRoutes";
 
 const app = express();
 
 connection();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", router);
 
 app.listen(myEnvVariables.PORT, () => {
   console.log(`server running on http://localhost:${myEnvVariables.PORT}`);
