7afab27edf13be91c826b05086f1d806d35dd0cd
Welcome ! <usmanubaid9@gmail.com>
2023-12-04 17:42:18 +0100

    feat: add user to database

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,9 +1,29 @@
 import { Request, Response } from "express";
+import { client } from "../db/dbConnection";
+import { User } from "./types/User";
 
 export const userSignin = (req: Request, res: Response) => {
   res.json({ message: "User Sign in" });
 };
 
-export const userSignup = (req: Request, res: Response) => {
-  res.json({ message: "User Sign up" });
+export const userSignup = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password }: User = req.body;
+
+    if (!username || !email || !password) {
+      res.status(400).json({ message: "Please fill all the fields." });
+    } else {
+      const db = client.db("user-auth");
+      const collection = db.collection("users");
+      const user = { username, email, password };
+      const result = await collection.insertOne(user);
+
+      res.json({ message: result });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while creating the user." });
+  }
 };
--- backend/src/db/dbConnection.ts
+++ backend/src/db/dbConnection.ts
@@ -1,9 +1,10 @@
 import { MongoClient } from "mongodb";
 import { myEnvVariables } from "./env";
 
+export const client = new MongoClient(myEnvVariables.DATABASE_URL);
+
 export const connection = async () => {
   try {
-    const client = new MongoClient(myEnvVariables.DATABASE_URL);
     await client.connect();
     console.log("Database Connected");
   } catch (error) {
