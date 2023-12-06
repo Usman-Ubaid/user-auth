70e185ad6f22b35d58d491d6a6bdff7b78c12fe1
Welcome ! <usmanubaid9@gmail.com>
2023-12-06 20:39:50 +0100

    feat: generate JWT

--- backend/src/module/auth.ts
+++ backend/src/module/auth.ts
@@ -1,4 +1,7 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { myEnvVariables } from "../db/env";
+import { ObjectId } from "mongodb";
 
 const saltRounds = 10;
 
@@ -8,4 +11,8 @@ export const hashPassword = (password: string) => {
 
 export const comparePasswords = (userPassword: string, dbPassword: string) => {
   return bcrypt.compare(userPassword, dbPassword);
+};
+
+export const generateJWT = (id: ObjectId) => {
+  return jwt.sign({ id }, myEnvVariables.JWT_SECRET);
 };
