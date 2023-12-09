da0fc06cc23e00b2b820266d6c1aaf70c798a13c
Welcome ! <usmanubaid9@gmail.com>
2023-12-09 18:12:10 +0100

    use reusable form components in sigin page

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,9 +1,15 @@
 import Layout from "../components/Layout";
+import Fieldset from "../components/formComponents/Fieldset";
 
 const Signin = () => {
   return (
     <Layout>
-      <div>Signin</div>
+      <div className="form-wrapper">
+        <form className="form">
+          <Fieldset legend="Signin" />
+          <input type="submit" value="Signin" />
+        </form>
+      </div>
     </Layout>
   );
 };
