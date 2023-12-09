9bb52ec8ce5988adfb61215bde12b5670fc6bb2e
Welcome ! <usmanubaid9@gmail.com>
2023-12-09 15:24:30 +0100

    refactor Routers component

--- frontend/src/routes/Routers.tsx
+++ frontend/src/routes/Routers.tsx
@@ -1,10 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
 
 const Routers = () => {
+  const routes = [
+    { id: 1, path: "/signin", element: <Signin /> },
+    { id: 2, path: "/signup", element: <Signup /> },
+  ];
   return (
     <Routes>
-      <Route path="/signin" />
-      <Route path="/signin" />
+      {routes.map((route) => (
+        <Route key={route.id} path={route.path} element={route.element} />
+      ))}
     </Routes>
   );
 };
