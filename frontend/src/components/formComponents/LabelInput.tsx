944c7be8eeea3dfdf1b27c74b6faba15dac377f0
Welcome ! <usmanubaid9@gmail.com>
2023-12-11 00:40:31 +0100

    use useForm hook in Signin and LabelInput

--- frontend/src/components/formComponents/LabelInput.tsx
+++ frontend/src/components/formComponents/LabelInput.tsx
@@ -2,12 +2,19 @@ type LabelInputProps = {
   label: string;
   name: string;
   id: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const LabelInput = ({ label, name, id }: LabelInputProps) => {
+const LabelInput = ({ label, name, id, onChange }: LabelInputProps) => {
   return (
     <div className="input-wrapper">
-      <input type="text" name={name} id={id} placeholder={label} />
+      <input
+        type="text"
+        name={name}
+        id={id}
+        placeholder={label}
+        onChange={onChange}
+      />
       <label htmlFor={name}>{label}</label>
     </div>
   );
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,16 +1,42 @@
 import Layout from "../components/Layout";
 import LabelInput from "../components/formComponents/LabelInput";
+import { useForm } from "../hooks/useForm";
+
+export type SigninFormState = {
+  email: string;
+  password: string;
+};
 
 const Signin = () => {
+  const { formData, handleInputChange } = useForm<SigninFormState>({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form Data: ", formData);
+  };
+
   return (
     <Layout>
       <div className="form-wrapper">
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <div>
             <h2>Sign In</h2>
           </div>
-          <LabelInput label="Email" name="email" id="email" />
-          <LabelInput label="Password" name="password" id="password" />
+          <LabelInput
+            label="Email"
+            name="email"
+            id="email"
+            onChange={handleInputChange}
+          />
+          <LabelInput
+            label="Password"
+            name="password"
+            id="password"
+            onChange={handleInputChange}
+          />
           <button className="btn" type="submit">
             Login
           </button>
