7d4acdc7cbe5bbcc8e861624a6175777792c9e22
Welcome ! <usmanubaid9@gmail.com>
2023-12-10 20:24:10 +0100

    develop sign up form

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,10 +1,23 @@
 import Layout from "../components/Layout";
+import LabelInput from "../components/formComponents/LabelInput";
 
 const Signup = () => {
   return (
     <div>
       <Layout>
-        <div>Signup</div>
+        <div className="form-wrapper">
+          <form className="form">
+            <div>
+              <h2>Sign Up</h2>
+            </div>
+            <LabelInput label="Username" name="username" id="username" />
+            <LabelInput label="Email" name="email" id="email" />
+            <LabelInput label="Password" name="password" id="password" />
+            <button className="btn" type="submit">
+              Register
+            </button>
+          </form>
+        </div>
       </Layout>
     </div>
   );
