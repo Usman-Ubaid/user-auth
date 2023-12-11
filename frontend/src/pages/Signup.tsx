4b01c302e4f00b2f1e20d98a86a1e6f2bc83ac83
Welcome ! <usmanubaid9@gmail.com>
2023-12-11 14:57:56 +0100

    feat: use useForm hook

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,18 +1,50 @@
 import Layout from "../components/Layout";
 import LabelInput from "../components/formComponents/LabelInput";
+import { useForm } from "../hooks/useForm";
+
+export type SignupFormState = {
+  username: "";
+  email: string;
+  password: string;
+};
 
 const Signup = () => {
+  const { formData, handleInputChange } = useForm<SignupFormState>({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("FormData: ", formData);
+  };
   return (
     <div>
       <Layout>
         <div className="form-wrapper">
-          <form className="form">
+          <form className="form" onSubmit={handleSubmit}>
             <div>
               <h2>Sign Up</h2>
             </div>
-            <LabelInput label="Username" name="username" id="username" />
-            <LabelInput label="Email" name="email" id="email" />
-            <LabelInput label="Password" name="password" id="password" />
+            <LabelInput
+              label="Username"
+              name="username"
+              id="username"
+              onChange={handleInputChange}
+            />
+            <LabelInput
+              label="Email"
+              name="email"
+              id="email"
+              onChange={handleInputChange}
+            />
+            <LabelInput
+              label="Password"
+              name="password"
+              id="password"
+              onChange={handleInputChange}
+            />
             <button className="btn" type="submit">
               Register
             </button>
