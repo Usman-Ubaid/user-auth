8e3c2b07be6c1b1b0680c088975499661965df62
Welcome ! <usmanubaid9@gmail.com>
2023-12-12 18:03:24 +0100

    add changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,13 +7,11 @@ import { router as dashboardRouter } from "./routes/dashboardRoute";
 
 const app = express();
 
-const port = myEnvVariables.PORT || 5000;
-
 connection();
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", userRouter);
 app.use("/api", dashboardRouter);
@@ -22,6 +20,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Server");
 });
 
-app.listen(port, () => {
-  console.log(`server running on PORT: http://localhost:${port}`);
+app.listen(myEnvVariables.PORT, () => {
+  console.log(`server running on PORT:${myEnvVariables.PORT}`);
 });
