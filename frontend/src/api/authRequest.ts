bfd0e2120ba6e27caff2e232ab78f61537e221e6
Welcome ! <usmanubaid9@gmail.com>
2023-12-12 20:46:34 +0100

    fix error

--- frontend/src/api/authRequest.ts
+++ frontend/src/api/authRequest.ts
@@ -5,16 +5,13 @@ export const signinRequest = async <T>(
   resource: string
 ): Promise<ResponseType | undefined> => {
   try {
-    const postData = await fetch(
-      `${import.meta.env.VITE_API_KEY}/${resource}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      }
-    );
+    const postData = await fetch(`${import.meta.env.VITE_API_KEY}${resource}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
 
     const response: ResponseType = await postData.json();
     return response;
