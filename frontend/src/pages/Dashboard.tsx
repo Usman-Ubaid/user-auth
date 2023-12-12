eca276adcda85dc7bdf41e91ca0eef4e0db61a82
Welcome ! <usmanubaid9@gmail.com>
2023-12-12 11:07:01 +0100

    develop dashboard and style it

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,14 @@
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <nav>
+        <button className="btn">Logout</button>
+      </nav>
+      <section className="body">
+        <h1>Thank You for Signing in</h1>
+      </section>
+    </div>
+  );
 };
 
 export default Dashboard;
